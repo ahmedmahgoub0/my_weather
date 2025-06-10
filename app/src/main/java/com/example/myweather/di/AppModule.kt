0e7b511f077334803408c27ecc2cf5ac3cf654b6
Ahmed Mahgoub <aed7862@gmail.com>
2025-06-10 19:32:45 +0300

    feat: edit setup of ktor client

--- app/src/main/java/com/example/myweather/di/AppModule.kt
+++ app/src/main/java/com/example/myweather/di/AppModule.kt
@@ -1,21 +1,34 @@
 package com.example.myweather.di
 
 import android.content.Context
-import com.example.myweather.data.datasource.HttpClientFactory
+import com.example.myweather.data.datasource.RemoteDataSource
 import com.example.myweather.data.datasource.RemoteDataSourceImpl
 import com.example.myweather.data.repository.LocationRepository
 import com.example.myweather.data.repository.LocationRepositoryImpl
-import com.example.myweather.data.datasource.RemoteDataSource
 import com.example.myweather.data.repository.WeatherRepository
 import com.example.myweather.data.repository.WeatherRepositoryImpl
 import com.example.myweather.ui.weatherscreen.WeatherViewModel
 import com.google.android.gms.location.LocationServices
+import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single { HttpClientFactory.onCreate(CIO.create()) }
+    single {
+        HttpClient(CIO.create()) {
+            install(ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+    }
     single { LocationServices.getFusedLocationProviderClient(get<Context>()) }
 
     single<RemoteDataSource> { RemoteDataSourceImpl(get()) }
